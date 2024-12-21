d76f7dc9f1e2e8869d31f7660f6f06c6a50181f6
misim3 <sim000314@naver.com>
2024-12-21 10:58:03 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/misim/mitube_v1/MitubeV1Application.java
@@ -0,0 +1,13 @@
+package com.misim.mitube_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitubeV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MitubeV1Application.class, args);
+	}
+
+}
