e6c1235d448a83fc70fff219b0d169f30e9cad98
misim3 <sim000314@naver.com>
2024-12-22 17:44:18 +0900

    refactor: 비디오 메타데이터 레포지토리의 대상 클래스 변경

--- src/main/java/com/misim/mitube_v1/VideoMetadataRepository.java
+++ src/main/java/com/misim/mitube_v1/VideoMetadataRepository.java
@@ -1,8 +1,7 @@
 package com.misim.mitube_v1;
 
-import com.misim.mitube_v1.domain.VideoMetadata;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VideoMetadataRepository extends JpaRepository<VideoMetadata, Long> {
+public interface VideoMetadataRepository extends JpaRepository<VideoMetadataEntity, Long> {
 
 }
