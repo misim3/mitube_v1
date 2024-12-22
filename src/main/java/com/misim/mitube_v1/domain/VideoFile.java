a6cc0460b92b6dd5b03bf2650ee7230ddeeacd3b
misim3 <sim000314@naver.com>
2024-12-22 18:31:56 +0900

    style: 비디오 파일 도메인 클래스 내 불필요한 import 문 삭제

--- src/main/java/com/misim/mitube_v1/domain/VideoFile.java
+++ src/main/java/com/misim/mitube_v1/domain/VideoFile.java
@@ -1,7 +1,4 @@
 package com.misim.mitube_v1.domain;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 public record VideoFile(long fileSize, String fileName, String filePath, String mimeType) {
 }
