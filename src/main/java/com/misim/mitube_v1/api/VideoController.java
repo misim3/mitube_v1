8eaca3e41bd857f5c4f4377a53cede05b6ea72f0
misim3 <sim000314@naver.com>
2024-12-25 16:06:50 +0900

    feat: 비디오 컨트롤러 초기 구현
    Signed-off-by: misim3 <sim000314@naver.com>

---
+++ src/main/java/com/misim/mitube_v1/api/VideoController.java
@@ -0,0 +1,51 @@
+package com.misim.mitube_v1.api;
+
+import com.misim.mitube_v1.VideoService;
+import com.misim.mitube_v1.domain.VideoMetadata;
+import java.net.URI;
+import java.util.List;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+public class VideoController {
+
+    private final VideoService videoService;
+
+    VideoController(VideoService videoService) {
+        this.videoService = videoService;
+    }
+
+    @PostMapping("/videos")
+    public ResponseEntity<?> videoUpload(MultipartFile file) {
+
+        String id = videoService.upload(file);
+
+        return ResponseEntity.created(URI.create("/videos/" + id)).build();
+
+    }
+
+    @GetMapping("/videos/{id}")
+    public ResponseEntity<?> videoStream(@PathVariable long id) {
+
+        Resource resource = videoService.stream(id);
+
+        return ResponseEntity.ok().body(resource);
+
+    }
+
+    @GetMapping("/videos")
+    public ResponseEntity<?> videoList() {
+
+        List<VideoMetadata> videoMetadataList = videoService.getList();
+
+        return ResponseEntity.ok().body(videoMetadataList);
+
+    }
+
+}
