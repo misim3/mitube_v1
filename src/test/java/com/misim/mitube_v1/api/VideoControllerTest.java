3b7a72dea98b4b84e8fb6c52eef570c00540f428
misim3 <sim000314@naver.com>
2024-12-28 08:54:36 +0900

    feat: 컨트롤러 통합 테스트 초기 구현
    Signed-off-by: misim3 <sim000314@naver.com>

---
+++ src/test/java/com/misim/mitube_v1/api/VideoControllerTest.java
@@ -0,0 +1,23 @@
+package com.misim.mitube_v1.api;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class VideoControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void videoUpload() {
+    }
+
+    @Test
+    void videoStream() {
+    }
+
+    @Test
+    void videoList() {
+    }
+}
